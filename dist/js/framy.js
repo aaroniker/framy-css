fd7452f737a251448f46c44ad4edc54af9ac3a09
Aaron Iker <mail@aaroniker.me>
2017-09-27 01:33:40 +0200

    remove jquery dependency, remove shitty tooltips

--- dist/js/framy.js
+++ dist/js/framy.js
@@ -1,77 +1,122 @@
-jQuery(document).ready(function($) {
-
-    $('.form-group input.form-field').focus(function(e) {
-        $(this).parent().addClass('focus');
+[].forEach.call(document.querySelectorAll('.form-group input.form-field'), function(el) {
+    el.addEventListener('focus', function() {
+        el.parentNode.classList.add('focus');
     });
-
-    $('.form-group input.form-field').blur(function(e) {
-        $(this).parent().removeClass('focus');
+    el.addEventListener('blur', function() {
+        el.parentNode.classList.remove('focus');
     });
+});
 
-    $('.form-group > span').click(function(e) {
-        $(this).parent().children('input').focus();
-    });
-
-    $('.password .icon-view').click(function(e) {
-        if(!$(this).hasClass('active')) {
-            $(this).parent().children('input').attr('type', 'text').focus();
+[].forEach.call(document.querySelectorAll('.password .icon-view'), function(el) {
+    el.addEventListener('click', function() {
+        var input = el.parentNode.getElementsByTagName('input')[0];
+        if(!el.classList.contains('active')) {
+            input.type = 'text';
         } else {
-            $(this).parent().children('input').attr('type', 'password').focus();
+            input.type = 'password';
         }
-        $(this).toggleClass('active');
+        input.focus();
+        el.classList.toggle('active');
     });
+});
 
-    $('select.form-select').each(function(e) {
+[].forEach.call(document.querySelectorAll('select.form-select'), function(el) {
 
-        var classes = $(this).attr('class'),
-        id = $(this).attr('id'),
-        name = $(this).attr('name'),
-        value = $(this).find(':selected').text();
+    var classes = el.classList.value,
+    id = el.id,
+    name = el.name,
+    value = el.options[el.selectedIndex].textContent,
+    i;
 
-        var template =  '<div class="' + classes + '">';
-        template += '<span>' + value + '</span>';
-        template += '<div>';
-        $(this).find('option').each(function() {
-            template += '<span class="' + $(this).attr('class') + '" data-value="' + $(this).attr('value') + '">' + $(this).html() + '</span>';
-        });
-        template += '</div></div>';
+    var wrapper = document.createElement('div');
+    var template = '<span>' + value + '</span>';
+    template += '<div>';
+    for(i = 0; i < el.options.length; i++) {
+        template += '<span class="' + el.options[i].classList.value + '" data-value="' + el.options[i].value + '">' + el.options[i].innerHTML + '</span>';
+    }
+    template += '</div>';
 
-        $(this).hide();
-        $(this).before(template);
+    wrapper.className = classes;
+    wrapper.innerHTML = template;
 
-    });
-
-    $('.form-select:not(.disabled) > span').on('click', function(e) {
-        $('html').one('click',function(e) {
-            $('.form-select').removeClass('open');
-        });
-        $(this).parent().toggleClass('open');
-        e.stopPropagation();
-    });
-
-    $('.form-select > div > span').on('click', function(e) {
-        var div = $(this).parent().parent();
-        var select = div.next('select');
-        select.val($(this).data('value')).trigger('change');
-        $(this).parent().children('span').removeClass('active');
-        $(this).addClass('active');
-        div.removeClass('open').children('span').text($(this).text());
-    });
-
-    $('[data-tooltip]').hover(function(e){
-        var title = $(this).data('tooltip');
-        $('<div/>').addClass('tooltip').text(title).appendTo('body').fadeIn(150);
-    }, function() {
-        $('.tooltip').fadeOut(150, function(e) {
-            $(this).remove();
-        });
-    }).mousemove(function(e) {
-        var mousex = e.pageX + 12,
-        mousey = e.pageY + 8;
-        $('.tooltip').css({
-            top: mousey,
-            left: mousex
-        });
-    });
+    el.style.display = 'none';
+    el.parentNode.insertBefore(wrapper, el);
 
 });
+
+[].forEach.call(document.querySelectorAll('.form-select:not(.disabled) > span'), function(el) {
+    el.addEventListener('click', function(e) {
+        document.addEventListener('click', function() {
+            [].forEach.call(document.querySelectorAll('.form-select'), function(allFS) {
+                allFS.classList.remove('open');
+            });
+        });
+        el.parentNode.classList.toggle('open');
+        e.stopPropagation();
+    });
+});
+
+[].forEach.call(document.querySelectorAll('.form-select > div > span'), function(el) {
+    el.addEventListener('click', function(e) {
+        var div = el.parentNode.parentNode;
+        var select = div.nextSibling;
+        select.value = el.dataset.value;
+        select.dispatchEvent(new Event('change'));
+        var entries = el.parentNode.getElementsByTagName('span');
+        for(i = 0; i < entries.length; i++) {
+            entries[i].classList.remove('active');
+        }
+        el.classList.add('active');
+        div.classList.remove('open');
+        div.getElementsByTagName('span')[0].textContent = el.textContent;
+    });
+});
+
+// classList Polyfill - Source: https://gist.github.com/k-gun/c2ea7c49edf7b757fe9561ba37cb19ca
+;(function() {
+    var regExp = function(name) {
+        return new RegExp('(^| )'+ name +'( |$)');
+    };
+    var forEach = function(list, fn, scope) {
+        for (var i = 0; i < list.length; i++) {
+            fn.call(scope, list[i]);
+        }
+    };
+    function ClassList(element) {
+        this.element = element;
+    }
+    ClassList.prototype = {
+        add: function() {
+            forEach(arguments, function(name) {
+                if (!this.contains(name)) {
+                    this.element.className += ' '+ name;
+                }
+            }, this);
+        },
+        remove: function() {
+            forEach(arguments, function(name) {
+                this.element.className =
+                    this.element.className.replace(regExp(name), '');
+            }, this);
+        },
+        toggle: function(name) {
+            return this.contains(name) ? (this.remove(name), false) : (this.add(name), true);
+        },
+        contains: function(name) {
+            return regExp(name).test(this.element.className);
+        },
+        replace: function(oldName, newName) {
+            this.remove(oldName), this.add(newName);
+        }
+    };
+    if(!('classList' in Element.prototype)) {
+        Object.defineProperty(Element.prototype, 'classList', {
+            get: function() {
+                return new ClassList(this);
+            }
+        });
+    }
+    if(window.DOMTokenList && DOMTokenList.prototype.replace == null) {
+        DOMTokenList.prototype.replace = ClassList.prototype.replace;
+    }
+})();
